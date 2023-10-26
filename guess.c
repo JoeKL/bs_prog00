641521fb9c40cd9fa471d76eff5254f9cc57a98b
Niko Tepe <niko.tepe@gmail.com>
2023-10-26 15:35:51 +0200

    reading memory address at target

--- guess.c
+++ guess.c
@@ -1,21 +1,38 @@
 #include "guessing.h"
 #include <math.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+unsigned long get_data_segment_start() {
+    FILE *maps_file = fopen("/proc/self/maps", "r");
+
+    unsigned long start, end;
+    char perms[5];
+    while (fscanf(maps_file, "%lx-%lx %4s", &start, &end, perms) == 3) {
+        if (perms[0] == 'r' && perms[1] == 'w') {
+            fclose(maps_file);
+            return start;
+        }
+        // Skip to the next line
+        while (fgetc(maps_file) != '\n' && !feof(maps_file));
+    }
+
+    fclose(maps_file);
+    fprintf(stderr, "Data segment not found\n");
+	return -1;
+}
+
 unsigned int guess_the_number(void) //lt. Aufgabenstellung darf dieser Header nicht verändert werden.
 {
 
-    int left = 0;
-    int right = 99999;
-    while (left <= right){
-		int mid = round((left + right)/2);
-				if (evaluate(mid) == 0){
-					return mid;
-				} else if (evaluate(mid) == 1) {	// evaluate == 1 wenn mid < target
-					left = mid + 1;
-				} else if (evaluate(mid) == -1) {	// evaluate == -1 wenn mid > target
-					right = mid - 1;
-				}
-	}
-	return -1;
+	unsigned long process__data_start = get_data_segment_start();
+	unsigned long target_address = process__data_start + 0x44;
+
+	int value;
+	memcpy(&value, (void *)target_address, sizeof(int));
+	return value;
 
 }
